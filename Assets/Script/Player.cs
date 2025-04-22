8dbc1fbfff165d25586d3f20801885a0819c84a2
Prem1751 <prempreeda098980@gmail.com>
2025-04-22 11:27:10 +0700

    walk

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,16 +1,52 @@
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed;
+    public float groundDist;
+
+    public LayerMask terrainLayer;
+    public Rigidbody rb;
+    public SpriteRenderer sr;
+
+    // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Ground detection and adjustment
+        RaycastHit hit;
+        Vector3 castPos = transform.position;
+        castPos.y += 1;
+
+        if (Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, terrainLayer))
+        {
+            if (hit.collider != null)
+            {
+                Vector3 movePos = transform.position;
+                movePos.y = hit.point.y + groundDist;
+                transform.position = movePos;
+            }
+        }
+
+        // Movement input
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 moveDir = new Vector3(x, 0, y);
+        rb.linearVelocity = moveDir * speed;
+
+        // Sprite flipping based on movement direction
+        if (x != 0 && x < 0)
+        {
+            sr.flipX = true;
+        }
+        else if (x != 0 && x > 0)
+        {
+            sr.flipX = false;
+        }
     }
 }
